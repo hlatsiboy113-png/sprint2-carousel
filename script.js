38181285c6c5f1d322ef755481baf084d190b64b
hlatsiboy113-png <hlatsiboy113@gmail.com>
2026-04-30 09:56:20 +0200

    Add languages data and state

---
+++ script.js
@@ -0,0 +1,19 @@
+const languages = [
+  {
+    name: "HTML",
+    image: "https://cdn-icons-png.flaticon.com/512/732/732212.png",
+    description: "Structure of web pages"
+  },
+  {
+    name: "CSS",
+    image: "https://cdn-icons-png.flaticon.com/512/732/732190.png",
+    description: "Styling and layout"
+  },
+  {
+    name: "JavaScript",
+    image: "https://cdn-icons-png.flaticon.com/512/5968/5968292.png",
+    description: "Adds interactivity"
+  }
+];
+
+let currentIndex = 0;
